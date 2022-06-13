94672c98c70d9b615e77692e7bed1447e2320bf0
keta1 <k@ketal.icu>
2022-06-13 21:58:25 +0800

    javafx
    Signed-off-by: keta1 <k@ketal.icu>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     id("java")
+    id("application")
+    id("org.openjfx.javafxplugin") version "0.0.13"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
@@ -17,10 +19,15 @@ java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
 
+javafx {
+    version = "14"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
 dependencies {
     implementation("org.xerial:sqlite-jdbc:3.36.0.3")
     implementation("org.mybatis:mybatis:3.5.10")
-    // implementation("org.slf4j:slf4j-simple:1.7.30")
+    implementation("com.jfoenix:jfoenix:9.0.10")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
